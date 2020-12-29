cc326dd0ef65b3e865aff5ea4d80e187d9e49166
nihilias <nihilias@icloud.com>
2020-12-30 00:29:17 +0100

    chore: create SemanticVersionComparable protocol

---
+++ Sources/SwiftVersionCheck/SemanticVersionComparable.swift
@@ -0,0 +1,89 @@
+protocol SemanticVersionComparable: Comparable {
+    var major: UInt { get }
+    var minor: UInt { get }
+    var patch: UInt { get }
+
+    var extensions: [String]? { get }
+}
+
+// MARK: - Comparable
+
+extension SemanticVersionComparable {
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        lhs.major == rhs.major &&
+        lhs.minor == rhs.minor &&
+        lhs.patch == rhs.patch &&
+          lhs.extensions?.isEmpty == rhs.extensions?.isEmpty
+    }
+
+    static func < (lhs: Self, rhs: Self) -> Bool {
+        guard lhs != rhs else { return false }
+
+        if lhs.major < rhs.major {
+            return true
+        } else if lhs.major == rhs.major, lhs.minor < rhs.minor {
+            return true
+        } else if lhs.major == rhs.major, lhs.minor == rhs.minor, lhs.patch < rhs.patch {
+            return true
+        } else if
+            lhs.major == rhs.major,
+            lhs.minor == rhs.minor,
+            lhs.patch == rhs.patch,
+            lhs.extensions != nil,
+            rhs.extensions == nil {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    static func <= (lhs: Self, rhs: Self) -> Bool {
+        lhs == rhs || lhs < rhs
+    }
+
+    static func > (lhs: Self, rhs: Self) -> Bool {
+        guard lhs != rhs else { return false }
+
+        if lhs.major > rhs.major {
+            return true
+        } else if lhs.major == rhs.major, lhs.minor > rhs.minor {
+            return true
+        } else if lhs.major == rhs.major, lhs.minor == rhs.minor, lhs.patch > rhs.patch {
+            return true
+        } else if
+            lhs.major == rhs.major,
+            lhs.minor == rhs.minor,
+            lhs.patch == rhs.patch,
+            lhs.extensions == nil,
+            rhs.extensions != nil {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    static func >= (lhs: Self, rhs: Self) -> Bool {
+        lhs == rhs || lhs > rhs
+    }
+}
+
+// MARK: - Accessor
+
+extension SemanticVersionComparable {
+    var absoluteString: String {
+        [versionCode, `extension`]
+            .compactMap { $0 }
+            .joined(separator: "-")
+    }
+
+    var versionCode: String {
+        [major, minor, patch]
+            .map(String.init)
+            .joined(separator: ".")
+    }
+
+    var `extension`: String? {
+        extensions?.joined(separator: ".")
+    }
+}
+
