2693d5316a92e3b1da25e34b932714fd55bd26ad
nihilias <nihilias@icloud.com>
2021-01-07 21:04:41 +0100

    feat: add example project

---
+++ Example/SemanticVersionCompareExample/SemanticVersionCompareExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SemanticVersionCompareExample
+//
+//  Created by Marius Hötten-Löns on 07.01.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
