8173abda30a86b6d24cec0fe3be0af4d0a55542c
mflknr <nihilias@icloud.com>
2021-03-30 22:17:29 +0200

    refactor: cleanup docs

--- Sources/SwiftVersionCompare/SemanticVersionComparable/SemanticVersionComparable+Hashable.swift
+++ Sources/SwiftVersionCompare/SemanticVersionComparable/SemanticVersionComparable+Hashable.swift
@@ -6,6 +6,10 @@
 //
 
 extension SemanticVersionComparable {
+    /// Conformance to `Hashable` protocol.
+    ///
+    /// - Note: Since build-meta-data are not considered in ranking semantic version, it won't be considered
+    ///         here either.
     public func hash(into hasher: inout Hasher) {
         hasher.combine(major)
         hasher.combine(minor)
